08a06ba2698a7a961ea253fcb4e34e32ba2c0d8f
bwajeremiah <bwajeremiah@gmail.com>
2026-09-25 11:23:58 -0400

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,14 @@
+namespace IceCream;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Driver;
+public partial class Form1 : Form
+{
+    var connectionString = ("mongodb+srv://bwajeremiah_db_user:<39Firehouse>@cluster0.hklay79.mongodb.net/?appName=Cluster0");
+
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
